4d9cf2899c0de6238ddca62af9673b119309aaf8
Tic Cosmin-Ioan <109898557+TicCosmin-Ioan@users.noreply.github.com>
2023-06-24 11:44:44 +0300

    Add files via upload

---
+++ JS/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    var btn = $(".button");
+  btn.click(function() {
+    btn.toggleClass("paused");
+    return false;
+  });
+});
+
+function audioInviz() {
+    var audio = document.getElementById("audio");
+    audio.style.opacity = "0";
+  
+}
+
